c4c3e408d55641570d3afe8ee1decd994a9eb431
livin <livinmathew99@gmail.com>
2017-08-13 18:23:13 +0530

    Packaged and Ready

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ from distutils.core import setup
 setup(
     name='LinuxDrop',
     version='0.1',
-    packages=[''],
+    packages=['sender', 'receiver'],
     url='https://github.com/Livin21/LinuxDrop',
     license='MIT',
     author='livin',
-    author_email='livin@lmntrx.com',
-    description='AirDrop alternative for Linux Systems'
+    author_email='livinmathew99@gmail.com',
+    description='Airdrop alternative for Linux <3'
 )
